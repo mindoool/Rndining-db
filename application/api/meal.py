439d49a1c013638d43bd019c1afbdb680205b22a
Minseok Kim <min3482@snu.ac.kr>
2016-04-02 23:58:45 +0900

    create meal ing

--- application/api/meal.py
+++ application/api/meal.py
@@ -7,23 +7,37 @@ from application.models.mixin import SerializableModelMixin
 from application.lib.rest.auth_helper import required_token
 
 
-@api.route('/store', methods=['POST'])
+@api.route('/meal', methods=['POST'])
 @required_token
-def create_store(request_user_id=None):
+def create_meal(request_user_id=None):
     """
     ms
     :param request_user_id:
     :return:
     """
     request_params = request.get_json()
-    request_params['userId'] = request_user_id
-    content = request_params.get('content')
+    day = request_params.get('day')
+    time = request_params.get('time')
+    category = request_params.get('category')
 
-    # content가 제대로 입력이 안된 경우
-    if content is None:
+    # day가 제대로 입력이 안된 경우
+    if day is None:
         return jsonify(
-            userMessage="As-is 내용을 기입해주세요."
+            userMessage="요일 정보를 기입해주세요."
         ), 400
+
+    # time이 제대로 입력이 안된 경우
+    if time is None:
+        return jsonify(
+            userMessage="시간 정보를 기입해주세요."
+        ), 400
+
+    # category가 제대로 입력이 안된 경우
+    if category is None:
+        return jsonify(
+            userMessage="카테고리 정보를 기입해주세요."
+        ), 400
+
 
     # 이미 등록되어있는지 확인
     q = db.session.query(AsIs).filter(AsIs.user_id == request_user_id, AsIs.content == content)
