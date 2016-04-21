48e77b4db5253bad6aab96a95d3851c57f583889
Minseok Kim <min3482@snu.ac.kr>
2016-04-21 23:17:30 +0900

    menu_date_relation create api 실제상황에 맞게 수정... 테스트 전

--- application/api/menu_date_relation.py
+++ application/api/menu_date_relation.py
@@ -4,6 +4,9 @@ from flask import request, jsonify
 from . import api
 from application import db
 from application.models.menu_date_relation import MenuDateRelation
+from application.models.meal_date import MealDate
+from application.models.menu import Menu
+from application.api.meal_date import create_meal_dates
 from application.models.mixin import SerializableModelMixin
 from application.lib.rest.auth_helper import required_token, required_admin
 
@@ -12,44 +15,81 @@ from application.lib.rest.auth_helper import required_token, required_admin
 @required_token
 def create_meal_date_relations():
     request_params = request.get_json()
-    menu_id = request_params.get('menuId')
-    meal_date_id = request_params.get('mealDateId')
+    meal_id = request_params.get('mealId')
+    date = request_params.get('date').split('-')
+    date_object = datetime.date(int(date[0]), int(date[1]), int(date[2]))
+    menus = request_params.get('menus')
 
-    # meal_id가 제대로 입력이 안된 경우
-    if menu_id is None:
-        return jsonify(
-            userMessage="식단 정보를 기입해주세요."
-        ), 400
+    # 'rice', 'soup', 'maindish', 'sidedish', 'bread', 'drink', 'fruit', 'noodle', 'salad'
+    # rice = request_params.get('rice')
+    # soup = request_params.get('soup')
+    # maindish = request_params.get('maindish')
+    # sidedish = request_params.get('sidedish')
+    # bread = request_params.get('bread')
+    # drink = request_params.get('drink')
+    # fruit = request_params.get('fruit')
+    # noodle = request_params.get('noodle')
+    # salad = request_params.get('salad')
 
-    # meal_date_id가 제대로 입력이 안된 경우
-    if meal_date_id is None:
+    # date, meal_id가 제대로 입력이 안된 경우
+    if date is None:
         return jsonify(
-            userMessage="식단 날짜 정보를 기입해주세요."
+            userMessage="날짜를 입력해주세요."
         ), 400
+    elif meal_id is None:
+        return jsonify(
+            userMessage="식단을 입력해주세요."
+        )
+    else:
+        meal_date = db.session.query(MealDate).filter(MealDate.date == date_object, MealDate.meal_id == meal_id)
+        if meal_date.count() > 0:
+            meal_date_id = meal_date.id
+        else:
+            meal_date = MealDate(date=date_object, meal_id=meal_id)
+            db.session.add(meal_date)
+            db.session.commit()
+            meal_date_id = meal_date.id
+
+    # menus가 제대로 입력이 안된 경우
+    menu_id_list = []
+    if menus is None:
+        return jsonify(
+            userMessage="메뉴를 입력해주세요."
+        )
+    else:
+        for key in menus.keys():
+            menu = db.session.query(Menu).filter(Menu.name == menus[key])
+            if menu.count() > 0:
+                menu_id_list.append(menu.id)
+            else:
+                menu = Menu(name=menus[key], category=key)
+                db.session.add(menu)
+                db.session.commit()
+                menu_id_list.append(menu.id)
 
     # 이미 등록되어있는지 확인
-    q = db.session.query(MenuDateRelation).filter(MenuDateRelation.menu_id == menu_id,
-                                                  MenuDateRelation.meal_date_id == meal_date_id)
-    if q.count() > 0:
-        return jsonify(
-            userMessage="이미 등록되어있는 관계입니다."
-        ), 409
+    menu_date_relation_list = []
+    for menu_id in menu_id_list:
+        q = db.session.query(MenuDateRelation).filter(MenuDateRelation.menu_id == menu_id,
+                                                      MenuDateRelation.meal_date_id == meal_date_id)
+        if q.count() > 0:
+            return jsonify(
+                userMessage="이미 등록되어있는 관계입니다."
+            ), 409
 
-    try:
-        for key in request_params.keys():
-            request_params[SerializableModelMixin.to_snakecase(key)] = request_params.pop(key)
+        try:
+            menu_date_relation = MenuDateRelation(menu_id=menu_id, meal_date_id=meal_date_id)
+            db.session.add(menu_date_relation)
+            db.session.commit()
+            menu_date_relation_list.append(menu_date_relation.serialize())
+        except:
+            return jsonify(
+                userMessage="오류가 발생했습니다. 관리자에게 문의해주세요."
+            ), 403
 
-        menu_date_relation = MenuDateRelation(**request_params)
-        db.session.add(menu_date_relation)
-        db.session.commit()
-
-        return jsonify(
-            data=menu_date_relation.serialize()
+    return jsonify(
+            data=menu_date_relation_list
         ), 200
-    except:
-        return jsonify(
-            userMessage="오류가 발생했습니다. 관리자에게 문의해주세요."
-        ), 403
 
 
 # read 개별
