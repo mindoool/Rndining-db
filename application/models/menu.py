69e64f7f440ae99cf94b9dcd0ed20c9071059d36
Minseok Kim <min3482@snu.ac.kr>
2016-05-18 23:46:30 +0900

    db upgrade

--- application/models/menu.py
+++ application/models/menu.py
@@ -3,8 +3,10 @@ from application import db
 from application.models.mixin import TimeStampMixin
 from application.models.mixin import SerializableModelMixin
 
+
 class Menu(db.Model, TimeStampMixin, SerializableModelMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     like_count = db.Column(db.Integer, default=0)
-    category = db.Column(db.Enum('rice', 'soup', 'maindish', 'sidedish', 'bread', 'drink', 'fruit', 'noodle', 'salad'))
+    category = db.Column(
+        db.Enum('rice', 'soup', 'porridge', 'maindish', 'sidedish', 'bread', 'drink', 'fruit', 'noodle', 'salad', 'tea'))
