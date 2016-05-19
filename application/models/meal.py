150ab484eb7d6b84502c2e9f00ce0e16f43b7852
Minseok Kim <min3482@snu.ac.kr>
2016-05-19 23:21:09 +0900

    store db make

--- application/models/meal.py
+++ application/models/meal.py
@@ -3,8 +3,8 @@ from application import db
 from application.models.mixin import TimeStampMixin
 from application.models.mixin import SerializableModelMixin
 
-class Meal(db.Model, TimeStampMixin, SerializableModelMixin):
+
+class Store(db.Model, TimeStampMixin, SerializableModelMixin):
     id = db.Column(db.Integer, primary_key=True)
-    day = db.Column(db.Enum('mon', 'tue', 'wed', 'thu', 'fri'), nullable=False)
-    time = db.Column(db.Enum('morning', 'lunch', 'dinner'))
-    category = db.Column(db.Enum('noodle', 'salad', 'takeout', 'korean', 'western', 'dinner', 'dinner-noodle'))
+    name = db.Column(db.Enum('mon', 'tue', 'wed', 'thu', 'fri'), nullable=False)
+    category = db.Column(db.Enum('korean', 'western', 'soup', 'meat'))
